e1312bf3815392c0d3b526f195fa859856727c6d
Jason S Graham <jgraham20@gmail.com>
2017-03-08 14:01:33 -0600

    Fix org on windows

--- layers/jsg-org/config.el
+++ layers/jsg-org/config.el
@@ -25,9 +25,6 @@
          (setq org-agenda-files (list "/Users/Jason/Dropbox/Notes/org/church.org"
                                       "/Users/Jason/Dropbox/Notes/org/work.org"
                                       "/Users/Jason/Dropbox/Notes/org/projects.org")))
-        ;; (setq exec-path-from-shell-debug t)
-        ;; (exec-path-from-shell-copy-env "GOPATH")
-       ;;  (exec-path-from-shell-copy-env "GOHOME"))
         ((eq system-type 'gnu/linux)
          (setq org-directory ("/Users/Jason/Dropbox/Notes/org"))))
 
@@ -47,26 +44,9 @@
   (setq org-ellipsis "⬎")
 
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-
-  (let* ((variable-tuple (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
-                             ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
-                             ((x-list-fonts "Verdana")         '(:font "Verdana"))
-                             ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
-                             (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
-    (base-font-color     (face-foreground 'default nil 'default))
-    (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
-
-    (custom-theme-set-faces 'user
-                       `(org-level-8 ((t (,@headline ,@variable-tuple))))
-                       `(org-level-7 ((t (,@headline ,@variable-tuple))))
-                       `(org-level-6 ((t (,@headline ,@variable-tuple))))
-                       `(org-level-5 ((t (,@headline ,@variable-tuple))))
-                       `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
-                       `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
-                       `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.35))))
-                       `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.5))))
-                       `(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline nil)))))
-  )
+  (set-face-attribute 'org-table nil :inherit 'fixed-pitch)
+  (add-hook 'markdown-mode-hook (lambda () (variable-pitch-mode t)))
+  (add-hook 'org-mode-hook (lambda () (variable-pitch-mode t)))
 
   (setq org-default-notes-file (concat (file-name-as-directory org-directory) "notes.org"))
 
