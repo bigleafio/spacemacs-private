57542be7f4472abfbe09e617d7b56495a1f8206d
Jason S Graham <jgraham20@gmail.com>
2017-03-09 11:28:06 -0600

    Fix custom stuff

--- init.el
+++ init.el
@@ -279,14 +279,6 @@ This function is called at the very end of Spacemacs initialization."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-document-title ((t (:inherit default :weight bold :foreground "#c5c8c6" :font "Lucida Grande" :height 1.5 :underline nil))))
- '(org-level-1 ((t (:inherit default :weight bold :foreground "#c5c8c6" :font "Lucida Grande" :height 1.5))))
- '(org-level-2 ((t (:inherit default :weight bold :foreground "#c5c8c6" :font "Lucida Grande" :height 1.35))))
- '(org-level-3 ((t (:inherit default :weight bold :foreground "#c5c8c6" :font "Lucida Grande" :height 1.25))))
- '(org-level-4 ((t (:inherit default :weight bold :foreground "#c5c8c6" :font "Lucida Grande" :height 1.1))))
- '(org-level-5 ((t (:inherit default :weight bold :foreground "#c5c8c6" :font "Lucida Grande"))))
- '(org-level-6 ((t (:inherit default :weight bold :foreground "#c5c8c6" :font "Lucida Grande"))))
- '(org-level-7 ((t (:inherit default :weight bold :foreground "#c5c8c6" :font "Lucida Grande"))))
- '(org-level-8 ((t (:inherit default :weight bold :foreground "#c5c8c6" :font "Lucida Grande")))))
+ )
 )
 
