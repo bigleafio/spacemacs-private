946a854de5e281191ec583469563ef9655c291e3
Jason S Graham <jgraham20@gmail.com>
2017-06-12 11:31:46 -0500

    Fixes

--- init.el
+++ init.el
@@ -39,11 +39,9 @@ values."
      ;; ----------------------------------------------------------------
 
      helm
-     evil-commentary
-     evil-snipe
+     ;;evil-commentary
+     ;;evil-snipe
      auto-completion
-     (better-defaults :variables
-                 better-defaults-move-to-beginning-of-code-first t)
      (ibuffer :variables ibuffer-group-buffers-by 'projects)
      clojure
      emacs-lisp
@@ -54,40 +52,33 @@ values."
      version-control
      markdown
      theming
+     themes-megapack
      org
      (restclient :variables
                  restclient-use-org t)
      (shell :variables
              shell-default-height 30
              shell-default-position 'bottom)
-     (spell-checking :variables
-                     spell-checking-enable-by-default nil
-                     spell-checking-enable-auto-dictionary t)
-     syntax-checking
      version-control
-     frame-geometry
+     xkcd
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(
-                                      focus
-                                      keyfreq
+   dotspacemacs-additional-packages '(sublimity
+                                      mic-paren
+                                      color-theme-solarized
                                       org-sticky-header
                                       org-plus-contrib
                                       org-gcal
                                       calfw
-                                      dired-x
-                                      dired+
-                                      dired-ranger
-                                      dired-details
-                                      dired-toggle
+                                      lispy
      )
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
-   dotspacemacs-excluded-packages '()
+   dotspacemacs-excluded-packages '(highlight-parentheses)
    ;; Defines the behaviour of Spacemacs when installing packages.
    ;; Possible values are `used-only', `used-but-keep-unused' and `all'.
    ;; `used-only' installs only explicitly used packages and uninstall any
@@ -157,9 +148,9 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(gruvbox
+   dotspacemacs-themes '(solarized
                          leuven)
-   ;; If non nil the cursor color matches the state color in GUI Emacs.
+                                        ;gruvbox; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
@@ -339,14 +330,9 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
-  
+
   (setq inhibit-compacting-font-caches t)
 
-
-  ;; Don't litter my init file
-  (setq custom-file "~/.spacemacs.d/local/custom.el")
-  (load custom-file 'noerror)
-  ;;;;
   ;; Customization
   ;;;;
   (setq user-full-name "Jason Graham"
@@ -355,10 +341,6 @@ you should place your code here."
   (if (memq window-system '(w32))
       (setq dotspacemacs-smooth-scrolling nil))
 
-  ;(setq calendar-location-name "Bowling Green, KY"
-  ;      calendar-latitude 36.882678
-  ;      calendar-longitude -86.416158)
-
   ;; Add a directory to our load path so that when you `load` things
   ;; below, Emacs knows where to look for the corresponding file.
   (add-to-list 'load-path "~/.spacemacs.d/local/")
@@ -366,14 +348,19 @@ you should place your code here."
 
   (setq powerline-default-separator 'arrow)
 
-  (load "secret.el")
-
+  ;;(load "secret.el")
+  (require 'sublimity)
+  (require 'sublimity-scroll)
+  ;; (require 'sublimity-map)
+  ;; (require 'sublimity-attractive)
+  (sublimity-mode 1)
+  
   ;; Sets up exec-path-from shell
   ;; https://github.com/purcell/exec-path-from-shell
   (when (memq window-system '(mac ns))
     (exec-path-from-shell-initialize))
 
-  (jsg/configure-org-mode)
+  ;;(jsg/configure-org-mode)
 
   ;; Key binding to use "hippie expand" for text autocompletion
   ;; http://www.emacswiki.org/emacs/HippieExpand
@@ -407,7 +394,7 @@ you should place your code here."
 (setq delete-old-versions -1)
 (setq version-control t)
 (setq vc-make-backup-files t)
-(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/private/auto-save-list/" t)))
+;;(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/private/auto-save-list/" t)))
 
 (setq savehist-file "~/.emacs.d/private/savehist")
 (savehist-mode 1)
@@ -425,28 +412,42 @@ you should place your code here."
 
 ;; yay rainbows!
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(global-set-key (kbd "M-p") 'ace-window)
+(define-key smartparens-mode-map (kbd "C-<right>") 'sp-forward-slurp-sexp)
+(define-key smartparens-mode-map (kbd "C-<left>") 'sp-forward-barf-sexp)
+(define-key smartparens-mode-map (kbd "C-M-<left>") 'sp-backward-slurp-sexp)
+(define-key smartparens-mode-map (kbd "C-M-<right>") 'sp-backward-barf-sexp)
 
-;; use 2 spaces for tabs
-(defun die-tabs ()
-  (interactive)
-  (set-variable 'tab-width 2)
-  (mark-whole-buffer)
-  (untabify (region-beginning) (region-end))
-  (keyboard-quit))
+;; Get color-theme-solarized working. It is specified as an additional package
+;; above. First we setup some theme modifications - we must do this *before*
+;; we load the theme. Note that the color-theme-solarized package appears in
+;; the list of themes as plain old 'solarized'.
+(setq theming-modifications
+      '((solarized
+         ;; Provide a sort of "on-off" modeline whereby the current buffer has a nice
+         ;; bright blue background, and all the others are in cream.
+         ;; TODO: Change to use variables here. However, got error:
+         ;; (Spacemacs) Error in dotspacemacs/user-config: Wrong type argument: stringp, pd-blue
+         (mode-line :foreground "#e9e2cb" :background "#2075c7" :inverse-video nil)
+         (powerline-active1 :foreground "#e9e2cb" :background "#2075c7" :inverse-video nil)
+         (powerline-active2 :foreground "#e9e2cb" :background "#2075c7" :inverse-video nil)
+         (mode-line-inactive :foreground "#2075c7" :background "#e9e2cb" :inverse-video nil)
+         (powerline-inactive1 :foreground "#2075c7" :background "#e9e2cb" :inverse-video nil)
+         (powerline-inactive2 :foreground "#2075c7" :background "#e9e2cb" :inverse-video nil)
+         ;; Make a really prominent helm selection line.
+         (helm-selection :foreground "white" :background "red" :inverse-video nil)
+         ;; See comment above about dotspacemacs-colorize-cursor-according-to-state.
+         (cursor :background "#b58900")
+       )))
 
-  (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
-  (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
-  (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-  (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
-  (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
-  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-  (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+(set-terminal-parameter nil 'background-mode 'dark)
+(set-frame-parameter nil 'background-mode 'dark)
+(spacemacs/load-theme 'solarized)
 
-  ;; eldoc-mode shows documentation in the minibuffer when writing code
-  ;; http://www.emacswiki.org/emacs/ElDoc
-  (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
-  (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
-  (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+(setq blink-matching-paren nil)
+(paren-activate)
+(setq paren-match-face 'mode-line)
+
 )
 
 (defun jsg/configure-org-mode ()
@@ -583,7 +584,6 @@ you should place your code here."
       (setq org-confirm-babel-evaluate nil)
 
   )
-
 (defun dotspacemacs/emacs-custom-settings ()
   "Emacs custom settings.
 This is an auto-generated function, do not modify its content directly, use
@@ -596,7 +596,7 @@ This function is called at the very end of Spacemacs initialization."
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (evil-nerd-commenter define-word xterm-color ws-butler winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package unfill toc-org theme-changer symon sunshine string-inflection spaceline smeargle shell-pop reveal-in-osx-finder restclient-helm restart-emacs rase rainbow-delimiters popwin persp-mode pcre2el pbcopy paradox osx-trash osx-location osx-dictionary orgit org-sticky-header org-projectile org-present org-pomodoro org-gcal org-download org-bullets open-junk-file ob-restclient ob-http neotree mwim multi-term move-text mmm-mode markdown-toc magithub magit-gitflow magit-gh-pulls macrostep lorem-ipsum linum-relative link-hint launchctl keyfreq info+ indent-guide ibuffer-projectile hungry-delete htmlize hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-purpose helm-projectile helm-mode-manager helm-make helm-gitignore helm-flx helm-descbinds helm-company helm-c-yasnippet helm-ag gruvbox-theme google-translate golden-ratio gnuplot github-search github-clone gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gist gh-md fuzzy focus flyspell-correct-helm flycheck-pos-tip flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-snipe evil-search-highlight-persist evil-numbers evil-mc evil-matchit evil-magit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-commentary evil-args evil-anzu eshell-z eshell-prompt-extras esh-help elisp-slime-nav dumb-jump dired-toggle dired-ranger dired-details dired+ diff-hl deft company-statistics company-restclient column-enforce-mode clojure-snippets clj-refactor clean-aindent-mode cider-eval-sexp-fu calfw browse-at-remote auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line ac-ispell))))
+    (mic-paren color-theme-solarized color-theme sublimity xkcd zonokai-theme zenburn-theme zen-and-art-theme ws-butler winum white-sand-theme volatile-highlights vi-tilde-fringe uuidgen underwater-theme ujelly-theme twilight-theme twilight-bright-theme twilight-anti-bright-theme tronesque-theme toxi-theme tao-theme tangotango-theme tango-plus-theme tango-2-theme symon sunny-day-theme sublime-themes subatomic256-theme subatomic-theme string-inflection spaceline powerline spacegray-theme soothe-theme solarized-theme soft-stone-theme soft-morning-theme soft-charcoal-theme smyx-theme seti-theme reverse-theme restclient-helm restart-emacs rebecca-theme rainbow-delimiters railscasts-theme purple-haze-theme professional-theme popwin planet-theme phoenix-dark-pink-theme phoenix-dark-mono-theme persp-mode pastels-on-dark-theme password-generator paradox organic-green-theme org-sticky-header org-gcal request-deferred deferred open-junk-file omtose-phellack-theme oldlace-theme occidental-theme obsidian-theme ob-restclient ob-http noctilux-theme niflheim-theme neotree naquadah-theme mustang-theme move-text monokai-theme monochrome-theme molokai-theme moe-theme minimal-theme material-theme majapahit-theme magithub ghub+ apiwrap ghub magit-gh-pulls madhat2r-theme lush-theme lorem-ipsum lispy zoutline swiper ivy linum-relative link-hint light-soap-theme jbeans-theme jazz-theme ir-black-theme inkpot-theme info+ indent-guide ibuffer-projectile hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation hide-comnt heroku-theme hemisu-theme helm-purpose window-purpose imenu-list hc-zenburn-theme gruvbox-theme gruber-darker-theme grandshell-theme gotham-theme google-translate golden-ratio github-search github-clone gist gh marshal logito pcache ht gandalf-theme flx-ido flatui-theme flatland-theme firebelly-theme fill-column-indicator farmhouse-theme fancy-battery eyebrowse expand-region evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-lion evil-indent-plus evil-iedit-state iedit evil-exchange evil-ediff evil-args evil-anzu anzu espresso-theme editorconfig dumb-jump dracula-theme django-theme darktooth-theme darkokai-theme darkmine-theme darkburn-theme dakrone-theme cyberpunk-theme company-restclient restclient know-your-http-well column-enforce-mode color-theme-sanityinc-solarized clues-theme clean-aindent-mode cherry-blossom-theme calfw busybee-theme bubbleberry-theme birds-of-paradise-plus-theme badwolf-theme autothemer auto-highlight-symbol apropospriate-theme anti-zenburn-theme ample-zen-theme ample-theme alect-themes aggressive-indent afternoon-theme adaptive-wrap ace-link xterm-color which-key use-package unfill toc-org smeargle shell-pop selectric-mode sayid reveal-in-osx-finder pcre2el pbcopy osx-trash osx-dictionary orgit org-projectile org-present org-pomodoro org-download org-bullets mwim multi-term mmm-mode markdown-toc magit-gitflow macrostep launchctl keyfreq htmlize help-fns+ helm-themes helm-swoop helm-projectile helm-mode-manager helm-make helm-gitignore helm-flx helm-descbinds helm-company helm-c-yasnippet helm-ag gnuplot gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gh-md fuzzy focus flyspell-correct-helm flycheck-pos-tip exec-path-from-shell evil-visualstar evil-snipe evil-org evil-magit evil-escape evil-commentary eshell-z eshell-prompt-extras esh-help elisp-slime-nav diff-hl deft company-statistics color-theme-sanityinc-tomorrow clojure-snippets clj-refactor cider-eval-sexp-fu browse-at-remote bind-map auto-yasnippet auto-dictionary auto-compile ace-window ace-jump-helm-line ac-ispell))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
